23916cc726182c8a425ebbf4532cf5bcfbd80985
Sana <Lenovo@DESKTOP-I4OODFD>
2024-10-26 14:40:56 +0300

    Create - translateLetterPress func

--- scripts/game.js
+++ scripts/game.js
@@ -4,18 +4,28 @@ function ChooseWord() {
 }
 function displayDashes() {
   var secret_word = ChooseWord();
-
   var answer_section = document.getElementById("answer-section");
   var dashes = [];
-
   for (var i = 0; i < secret_word.length; i++) {
     dashes.push("_");
   }
-
   var answer = document.createElement("span");
   answer.innerHTML = dashes.join(" ");
-
   answer_section.appendChild(answer);
 }
 
+function translateLetterPress(letter_pressed) {
+  var letters = document.querySelectorAll(".letter");
+
+  console.log(letters);
+  for (var i = 0; i < letters.length; i++) {
+    letters[i].addEventListener("click", function () {
+      letter_pressed = this.innerHTML;
+      console.log(letter_pressed);
+    });
+  }
+  return letter_pressed;
+}
+var letter_pressed = "";
 displayDashes();
+translateLetterPress(letter_pressed);
